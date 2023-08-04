56ea6464b00a908324b5b517586eca9bca1d1020
RaptorZ98 <federicoheuercor@hotmail.com>
2023-08-03 20:45:30 -0700

    ON

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,5 +1,5 @@
 -- setup mysql
 CREATE DATABASE if NOT EXISTS hbnb_dev_db;
 CREATE USER if NOT EXISTS hbnb_dev@localhost IDENTIFIED BY 'hbnb_dev_pwd';
-GRANT ALL PRIVILEGES on hbnb_dev_db.* TO hbnb_dev@localhost;
-GRANT SELECT PRIVILEGES on performance_schema.* TO hbnb_dev@localhost;
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO hbnb_dev@localhost;
+GRANT SELECT PRIVILEGES ON performance_schema.* TO hbnb_dev@localhost;
--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,5 +1,5 @@
 -- setup mysql
 CREATE DATABASE if NOT EXISTS hbnb_test_db;
 CREATE USER if NOT EXISTS hbnb_test@localhost IDENTIFIED BY 'hbnb_test_pwd';
-GRANT ALL PRIVILEGES on hbnb_test_db.* TO hbnb_test@localhost;
-GRANT SELECT PRIVILEGES on performance_schema.* TO hbnb_test@localhost;
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO hbnb_test@localhost;
+GRANT SELECT PRIVILEGES ON performance_schema.* TO hbnb_test@localhost;
