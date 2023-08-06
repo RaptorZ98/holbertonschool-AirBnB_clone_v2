36d264e216b56ff70bfad555db703bad8ac60c67
RaptorZ98 <federicoheuercor@hotmail.com>
2023-08-06 16:04:32 -0700

    m

--- models/place.py
+++ models/place.py
@@ -1,17 +1,18 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
 import os
 from sqlalchemy import Column, Integer, String, DateTime, ForeignKey, Float
+from sqlalchemy.orm import relationship
 STORE = os.getenv('HBNB_TYPE_STORAGE')
 
 
-class Place(BaseModel):
+class Place(BaseModel, Base):
     """ A place to stay """
     if STORE == 'db':
         __tablename__ = 'places'
-        city_id = Column(String(60), ForeignKey('cities_id'))
-        user_id = Column(String(60), ForeignKey('users_id'))
+        city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
+        user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
         name = Column(String(128), nullable=False)
         description = Column(String(1024), nullable=True)
         number_rooms = Column(Integer, nullable=False, default=0)
@@ -20,6 +21,7 @@ class Place(BaseModel):
         price_by_night = Column(Integer, nullable=False, default=0)
         latitude = Column(Float, nullable=False)
         longitude = Column(Float, nullable=False)
+        amenity_ids = []
 
     else:
         city_id = ""
